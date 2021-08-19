acd32f1b8a54b84089d2fee446a4b706a5a72eda
Julia Brunett <julia.h.brunett@gmail.com>
2021-08-19 12:52:07 -0700

    Updated schemas

--- level-1/resources/schema.sql
+++ level-1/resources/schema.sql
@@ -1,32 +1,38 @@
+-- Reviews Table
 CREATE TABLE review_id_table (
   review_id TEXT PRIMARY KEY NOT NULL,
   customer_id INTEGER,
   product_id TEXT,
   product_parent INTEGER,
-  review_date DATE -- this should be in the formate yyyy-mm-dd
+  review_date DATE
 );
 
--- This table will contain only unique values
+-- Products Table
 CREATE TABLE products (
   product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
   product_title TEXT
 );
 
--- Customer table for first data set
+-- Customer Table
 CREATE TABLE customers (
   customer_id INT PRIMARY KEY NOT NULL UNIQUE,
   customer_count INT
 );
 
+-- Queries to see each table
 SELECT * FROM customers;
 SELECT * FROM products;
 SELECT * FROM review_id_table;
 
--- vine table
-CREATE TABLE vine_table (
-  review_id TEXT PRIMARY KEY,
-  star_rating INTEGER,
-  helpful_votes INTEGER,
-  total_votes INTEGER,
-  vine TEXT
-);
+-- Queries to reduce time
+SELECT * FROM review_id_table 
+LIMIT 100;
+SELECT * FROM customers
+LIMIT 100;
+SELECT * FROM products
+LIMIT 100;
+
+-- Drop tables to re-run
+DROP TABLE customers;
+DROP TABLE products;
+DROP TABLE review_id_table;
